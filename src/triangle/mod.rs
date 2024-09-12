ff9b657e875d8736a603dc48d89c1c2a41554185
Banyc <36535895+Banyc@users.noreply.github.com>
2024-09-12 14:32:02 +0800

    chore: texture LOB

--- src/triangle/mod.rs
+++ src/triangle/mod.rs
@@ -4,7 +4,6 @@ use std::{
 };
 
 use bytemuck_derive::{Pod, Zeroable};
-use image::GenericImageView;
 use num_traits::Float;
 use wgpu::util::DeviceExt;
 
@@ -51,31 +50,8 @@ struct Pipeline {
 }
 impl Pipeline {
     pub fn new(args: InitArgs<'_>) -> Self {
-        let (image, texture, texture_view) = texture(args.device);
-        let texture = wgpu::ImageCopyTexture {
-            texture: &texture,
-            mip_level: 0,
-            origin: wgpu::Origin3d::ZERO,
-            aspect: wgpu::TextureAspect::All,
-        };
-        let layout = wgpu::ImageDataLayout {
-            offset: 0,
-            bytes_per_row: Some(texture.texture.size().width * 4),
-            rows_per_image: None,
-        };
-        args.queue
-            .write_texture(texture, &image, layout, texture.texture.size());
-        let desc = wgpu::SamplerDescriptor {
-            label: Some("sampler"),
-            address_mode_u: wgpu::AddressMode::ClampToEdge,
-            address_mode_v: wgpu::AddressMode::ClampToEdge,
-            address_mode_w: wgpu::AddressMode::ClampToEdge,
-            mag_filter: wgpu::FilterMode::Linear,
-            min_filter: wgpu::FilterMode::Nearest,
-            mipmap_filter: wgpu::FilterMode::Linear,
-            ..Default::default()
-        };
-        let sampler = args.device.create_sampler(&desc);
+        let texture = Texture::new(args.device, WALL);
+        texture.register(args.queue);
         let shader = wgpu::ShaderSource::Wgsl(SHADER.into());
         let desc = wgpu::ShaderModuleDescriptor {
             label: None,
@@ -85,11 +61,7 @@ impl Pipeline {
         let vertex_buffer = wgpu::VertexBufferLayout {
             array_stride: core::mem::size_of::<VertexAttributes>() as wgpu::BufferAddress,
             step_mode: wgpu::VertexStepMode::Vertex,
-            attributes: &wgpu::vertex_attr_array![
-                0 => Float32x3,
-                1 => Float32x3,
-                2 => Float32x2,
-            ],
+            attributes: &VertexAttributes::layout(),
         };
         let vertex = wgpu::VertexState {
             module: &shader,
@@ -135,17 +107,13 @@ impl Pipeline {
                 wgpu::BindGroupLayoutEntry {
                     binding: 1,
                     visibility: wgpu::ShaderStages::FRAGMENT,
-                    ty: wgpu::BindingType::Texture {
-                        sample_type: wgpu::TextureSampleType::Float { filterable: true },
-                        view_dimension: wgpu::TextureViewDimension::D2,
-                        multisampled: false,
-                    },
+                    ty: texture.texture_layout(),
                     count: None,
                 },
                 wgpu::BindGroupLayoutEntry {
                     binding: 2,
                     visibility: wgpu::ShaderStages::FRAGMENT,
-                    ty: wgpu::BindingType::Sampler(wgpu::SamplerBindingType::Filtering),
+                    ty: texture.sampler_layout(),
                     count: None,
                 },
             ],
@@ -195,11 +163,11 @@ impl Pipeline {
                 },
                 wgpu::BindGroupEntry {
                     binding: 1,
-                    resource: wgpu::BindingResource::TextureView(&texture_view),
+                    resource: wgpu::BindingResource::TextureView(texture.view()),
                 },
                 wgpu::BindGroupEntry {
                     binding: 2,
-                    resource: wgpu::BindingResource::Sampler(&sampler),
+                    resource: wgpu::BindingResource::Sampler(texture.sampler()),
                 },
             ],
         };
@@ -273,6 +241,16 @@ struct VertexAttributes {
     pub position: [f32; 3],
     pub color: [f32; 3],
     pub tex_coord: [f32; 2],
+}
+impl VertexAttributes {
+    pub fn layout() -> Vec<wgpu::VertexAttribute> {
+        wgpu::vertex_attr_array![
+            0 => Float32x3,
+            1 => Float32x3,
+            2 => Float32x2,
+        ]
+        .into()
+    }
 }
 
 struct Mesh {
@@ -362,31 +340,84 @@ fn normalize_neg_pos_1<T: Float>(v: T) -> T {
     (v + one) / two
 }
 
-// struct Texture {
-//     texture: wgpu::Texture,
-//     dimensions: (u32, u32),
-// }
-fn texture(device: &wgpu::Device) -> (image::RgbaImage, wgpu::Texture, wgpu::TextureView) {
-    let wall = image::load_from_memory(WALL).unwrap().flipv();
-    let image = wall.to_rgba8();
-    let dimensions = wall.dimensions();
-    let texture_size = wgpu::Extent3d {
-        width: dimensions.0,
-        height: dimensions.1,
-        depth_or_array_layers: 1,
-    };
-    let desc = wgpu::TextureDescriptor {
-        label: Some("wall"),
-        size: texture_size,
-        mip_level_count: 1,
-        sample_count: 1,
-        dimension: wgpu::TextureDimension::D2,
-        format: wgpu::TextureFormat::Rgba8UnormSrgb,
-        usage: wgpu::TextureUsages::TEXTURE_BINDING | wgpu::TextureUsages::COPY_DST,
-        view_formats: &[],
-    };
-    let texture = device.create_texture(&desc);
-    let desc = wgpu::TextureViewDescriptor::default();
-    let view = texture.create_view(&desc);
-    (image, texture, view)
+#[derive(Debug)]
+struct Texture {
+    image: image::RgbaImage,
+    texture: wgpu::Texture,
+    view: wgpu::TextureView,
+    sampler: wgpu::Sampler,
+}
+impl Texture {
+    pub fn new(device: &wgpu::Device, bytes: &[u8]) -> Self {
+        let image = image::load_from_memory(bytes).unwrap().flipv();
+        let image = image.to_rgba8();
+        let (width, height) = image.dimensions();
+        let texture_size = wgpu::Extent3d {
+            width,
+            height,
+            depth_or_array_layers: 1,
+        };
+        let desc = wgpu::TextureDescriptor {
+            label: Some("wall"),
+            size: texture_size,
+            mip_level_count: 1,
+            sample_count: 1,
+            dimension: wgpu::TextureDimension::D2,
+            format: wgpu::TextureFormat::Rgba8UnormSrgb,
+            usage: wgpu::TextureUsages::TEXTURE_BINDING | wgpu::TextureUsages::COPY_DST,
+            view_formats: &[],
+        };
+        let texture = device.create_texture(&desc);
+        let desc = wgpu::TextureViewDescriptor::default();
+        let view = texture.create_view(&desc);
+        let desc = wgpu::SamplerDescriptor {
+            label: Some("sampler"),
+            address_mode_u: wgpu::AddressMode::ClampToEdge,
+            address_mode_v: wgpu::AddressMode::ClampToEdge,
+            address_mode_w: wgpu::AddressMode::ClampToEdge,
+            mag_filter: wgpu::FilterMode::Linear,
+            min_filter: wgpu::FilterMode::Nearest,
+            mipmap_filter: wgpu::FilterMode::Linear,
+            ..Default::default()
+        };
+        let sampler = device.create_sampler(&desc);
+        Self {
+            image,
+            texture,
+            view,
+            sampler,
+        }
+    }
+
+    pub fn register(&self, queue: &wgpu::Queue) {
+        let texture = wgpu::ImageCopyTexture {
+            texture: &self.texture,
+            mip_level: 0,
+            origin: wgpu::Origin3d::ZERO,
+            aspect: wgpu::TextureAspect::All,
+        };
+        let layout = wgpu::ImageDataLayout {
+            offset: 0,
+            bytes_per_row: Some(texture.texture.size().width * 4),
+            rows_per_image: None,
+        };
+        queue.write_texture(texture, &self.image, layout, texture.texture.size());
+    }
+
+    pub fn view(&self) -> &wgpu::TextureView {
+        &self.view
+    }
+    pub fn sampler(&self) -> &wgpu::Sampler {
+        &self.sampler
+    }
+    pub fn texture_layout(&self) -> wgpu::BindingType {
+        wgpu::BindingType::Texture {
+            sample_type: wgpu::TextureSampleType::Float { filterable: true },
+            view_dimension: wgpu::TextureViewDimension::D2,
+            multisampled: false,
+        }
+    }
+    pub fn sampler_layout(&self) -> wgpu::BindingType {
+        wgpu::BindingType::Sampler(wgpu::SamplerBindingType::Filtering)
+    }
 }
